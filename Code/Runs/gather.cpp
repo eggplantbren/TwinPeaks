0d3ed884afb133f9172b26409a33516db08b6edd
Brendon Brewer <brendon.brewer@gmail.com>
2014-10-26 21:46:24 +1300

    Calculate logZ and H from each run

--- Code/Runs/gather.cpp
+++ Code/Runs/gather.cpp
@@ -22,23 +22,15 @@ int main()
 	// Temperatures
 	double T1 = 0.1; double T2 = 1.;
 
-	// Normalising constant for prior weights
-	double logC = -1E300;
-
 	// Prior weights (relative) and 'likelihoods'
-	vector<double> logw, logL;
+	vector< vector<double> > logw, logL;
 
-	bool save = true;
-
+	// Directories to get runs from
 	vector<string> directories;
 	directories.push_back(string(".."));
 
-	fstream fout1, fout2;
-	if(save)
-	{
-		fout1.open("logw.txt", ios::out);
-		fout2.open("scalars.txt", ios::out);
-	}
+	// Output precision
+	cout<<setprecision(8);
 
 	int k = 0;
 	for(size_t i=0; i<directories.size(); i++)
@@ -52,15 +44,14 @@ int main()
 		double temp1, temp2, temp3;
 		while(fin1>>temp1 && fin2>>temp2 && fin2>>temp3)
 		{
-			if(save)
+			if(logw.size() == 0 || temp1 == logw[0][0])
 			{
-				fout1<<temp1<<endl;
-				fout2<<temp2<<' '<<temp3<<endl;
+				logw.push_back(vector<double>());
+				logL.push_back(vector<double>());
 			}
 
-			logC = logsumexp(logC, temp1);
-			logw.push_back(temp1);
-			logL.push_back(temp2/T1 + temp3/T2);
+			logw.back().push_back(temp1);
+			logL.back().push_back(temp2/T1 + temp3/T2);
 
 			k++;
 		}
@@ -71,67 +62,38 @@ int main()
 		cout<<"# Loaded "<<k<<" points."<<endl;
 	}
 
-	if(save)
+	// Normalise prior weights for each run
+	for(size_t i=0; i<logw.size(); i++)
 	{
-		fout1.close();
-		fout2.close();
+		double tot = -1E300;
+		for(size_t j=0; j<logw[i].size(); j++)
+			tot = logsumexp(tot, logw[i][j]);
+		for(size_t j=0; j<logw[i].size(); j++)
+			logw[i][j] -= tot;
 	}
 
-	// Calculate log(Z)
-	double logZ = -1E300;
-	for(size_t i=0; i<logL.size(); i++)
-		logZ = logsumexp(logZ, (logw[i] - logC) + logL[i]);
-
-	// Calculate partial log(Z)s from each run
-	double logw_max = *max_element(logw.begin(), logw.end());
-	vector<double> logZ_partial, logC_partial;
-	for(size_t i=0; i<logL.size(); i++)
+	// Use each run to calculate logZ
+	vector<double> logZ(logw.size(), -1E300);
+	for(size_t i=0; i<logw.size(); i++)
 	{
-		if(logw[i] == logw_max)
+		for(size_t j=0; j<logw[i].size(); j++)
+			logZ[i] = logsumexp(logZ[i], logw[i][j] + logL[i][j]);
+	}
+
+	// Use each run to calculate H
+	vector<double> H(logw.size(), 0.);
+	for(size_t i=0; i<logw.size(); i++)
+	{
+		for(size_t j=0; j<logw[i].size(); j++)
 		{
-			logZ_partial.push_back(-1E300);
-			logC_partial.push_back(-1E300);
+			H[i] += exp(logw[i][j] + logL[i][j] - logZ[i])
+					*(logL[i][j] - logZ[i]);
 		}
-		logZ_partial[logZ_partial.size() - 1] = logsumexp(logZ_partial[logZ_partial.size() - 1], logw[i] + logL[i]);
-		logC_partial[logC_partial.size() - 1] = logsumexp(logC_partial[logC_partial.size() - 1], logw[i]);
 	}
-	// Normalise within each run
-	for(size_t i=0; i<logZ_partial.size(); i++)
-		logZ_partial[i] -= logC_partial[i];
-	double max = *max_element(logZ_partial.begin(), logZ_partial.end());
-	vector<double> Z(logZ_partial.size());
-	double tot = 0.;
-	for(size_t i=0; i<logZ_partial.size(); i++)
-	{
-		Z[i] = exp(logZ_partial[i] - max);
-		tot += Z[i];
-	}
-	for(size_t i=0; i<logZ_partial.size(); i++)
-		Z[i] /= tot;
-	// Effective number of runs
-	double ENR = 0.;
-	for(size_t i=0; i<Z.size(); i++)
-		ENR += -Z[i]*log(Z[i] + 1E-300);
-	ENR = exp(ENR);
-	cout<<"# Number of runs = "<<Z.size()<<"."<<endl;
-	cout<<"# Effective number of contributing runs = "<<ENR<<"."<<endl;
 
-	
-	// Calculate H
-	double H = 0.;
-	double logP;
-	for(size_t i=0; i<logL.size(); i++)
-	{
-		// Posterior weight
-		logP = logw[i] - logC + logL[i] - logZ;
-		H += exp(logP)*logL[i];
-	}
-	H -= logZ;
-
-	// Output
-	cout<<setprecision(8);
-	cout<<"# ln(Z) = "<<logZ<<"."<<endl;
-	cout<<"# H = "<<H<<" nats."<<endl;
+	// Print estimates
+	for(size_t i=0; i<logw.size(); i++)
+		cout<<logZ[i]<<' '<<H[i]<<endl;
 
 	return 0;
 }
