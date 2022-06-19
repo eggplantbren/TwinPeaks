1a1be7cf5ca256397caa8b8c5f12dc3931b270f0
Brendon J. Brewer <brendon.brewer@gmail.com>
2022-06-20 09:17:23 +1200

    Wrote the class

--- src/Example.cpp
+++ src/Example.cpp
@@ -1,4 +1,6 @@
 #include "Example.h"
+#include "Tools/Misc.hpp"
+#include <sstream>
 
 namespace TwinPeaks
 {
@@ -8,6 +10,37 @@ Example::Example(Tools::RNG& rng)
 {
     for(double& x: xs)
         x = rng.rand();
+}
+
+
+double Example::perturb(Tools::RNG& rng)
+{
+    int k = rng.rand_int(size);
+    xs[k] += rng.randh();
+    Tools::wrap(xs[k]);
+    return 0.0;
+}
+
+std::vector<double> Example::scalars() const
+{
+    double f = 0.0;
+    double g = 0.0;
+
+    for(double x: xs)
+    {
+        f += -0.5*1E4*pow(x - 0.5, 2);
+        g += -1E2*x;
+    }
+
+    return {f, g};
+}
+
+std::string Example::to_string() const
+{
+    std::stringstream ss;
+    for(double x: xs)
+        ss << x << ' ';
+    return ss.str();
 }
 
 
